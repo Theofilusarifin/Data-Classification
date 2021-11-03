094bf18ecd0bcc78dac7a2f7c178295067b31312
Theofilusarifin <theofilusarifin@gmail.com>
2021-11-03 20:02:28 +0700

    Coba Push Victor

--- Project_Data_Mining/Project_Data_Mining/Form1.cs
+++ Project_Data_Mining/Project_Data_Mining/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_Data_Mining
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string victor = "haloo";
+        }
     }
 }
