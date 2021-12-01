50ae3de68b24575cd324377d1de8a6cd3ebfda41
Anri20 <hjayanata@gmail.com>
2021-12-01 23:08:33 +0700

    Proximity belum selesai

--- Project_Data_Mining/Project_Data_Mining/FormResult.Designer.cs
+++ Project_Data_Mining/Project_Data_Mining/FormResult.Designer.cs
@@ -29,12 +29,92 @@ namespace Project_Data_Mining
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.radioButtonManhattan = new System.Windows.Forms.RadioButton();
+            this.radioButtonEuclidean = new System.Windows.Forms.RadioButton();
+            this.radioButtonSupreme = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(90, 135);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(600, 239);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(344, 92);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(90, 23);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // radioButtonManhattan
+            // 
+            this.radioButtonManhattan.AutoSize = true;
+            this.radioButtonManhattan.Checked = true;
+            this.radioButtonManhattan.Location = new System.Drawing.Point(208, 51);
+            this.radioButtonManhattan.Name = "radioButtonManhattan";
+            this.radioButtonManhattan.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonManhattan.TabIndex = 2;
+            this.radioButtonManhattan.TabStop = true;
+            this.radioButtonManhattan.Text = "Manhattan";
+            this.radioButtonManhattan.UseVisualStyleBackColor = true;
+            this.radioButtonManhattan.Click += new System.EventHandler(this.radioButtonManhattan_Click);
+            // 
+            // radioButtonEuclidean
+            // 
+            this.radioButtonEuclidean.AutoSize = true;
+            this.radioButtonEuclidean.Location = new System.Drawing.Point(344, 51);
+            this.radioButtonEuclidean.Name = "radioButtonEuclidean";
+            this.radioButtonEuclidean.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonEuclidean.TabIndex = 3;
+            this.radioButtonEuclidean.Text = "Euclidean";
+            this.radioButtonEuclidean.UseVisualStyleBackColor = true;
+            this.radioButtonEuclidean.Click += new System.EventHandler(this.radioButtonEuclidean_Click);
+            // 
+            // radioButtonSupreme
+            // 
+            this.radioButtonSupreme.AutoSize = true;
+            this.radioButtonSupreme.Location = new System.Drawing.Point(478, 51);
+            this.radioButtonSupreme.Name = "radioButtonSupreme";
+            this.radioButtonSupreme.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonSupreme.TabIndex = 4;
+            this.radioButtonSupreme.Text = "Supreme";
+            this.radioButtonSupreme.UseVisualStyleBackColor = true;
+            this.radioButtonSupreme.Click += new System.EventHandler(this.radioButtonSupreme_Click);
+            // 
+            // FormResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonSupreme);
+            this.Controls.Add(this.radioButtonEuclidean);
+            this.Controls.Add(this.radioButtonManhattan);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "FormResult";
             this.Text = "FormResult";
+            this.Load += new System.EventHandler(this.FormResult_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.RadioButton radioButtonManhattan;
+        private System.Windows.Forms.RadioButton radioButtonEuclidean;
+        private System.Windows.Forms.RadioButton radioButtonSupreme;
     }
 }
--- Project_Data_Mining/Project_Data_Mining/FormResult.cs
+++ Project_Data_Mining/Project_Data_Mining/FormResult.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Project_Data_Mining_LIB;
+
 namespace Project_Data_Mining
 {
     public partial class FormResult : Form
@@ -15,6 +17,91 @@ namespace Project_Data_Mining
         public FormResult()
         {
             InitializeComponent();
+        }
+
+        #region RadioButton
+        private void radioButtonManhattan_Click(object sender, EventArgs e)
+        {
+            radioButtonEuclidean.Checked = false;
+            radioButtonSupreme.Checked = false;
+        }
+
+        private void radioButtonEuclidean_Click(object sender, EventArgs e)
+        {
+            radioButtonManhattan.Checked = false;
+            radioButtonSupreme.Checked = false;
+        }
+
+        private void radioButtonSupreme_Click(object sender, EventArgs e)
+        {
+            radioButtonManhattan.Checked = false;
+            radioButtonEuclidean.Checked = false;
+        }
+        #endregion
+
+        private void FormResult_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            double manhattan, euclidean, supreme;
+            double max;
+            List<double> Feat1 = new List<double>();
+            List<double> Feat2 = new List<double>();
+
+            #region Manhattan
+            if (radioButtonManhattan.Checked)
+            {
+                for (int x = 0; x < DataClass.DataMemberCount; x++)
+                {
+                    for (int y = 0; y < DataClass.DataMemberCount; y++)
+                    {
+                        manhattan = 
+                        Math.Abs(Convert.ToDouble(Feat1[x]) - Convert.ToDouble(Feat1[y])) + 
+                        Math.Abs(Convert.ToDouble(Feat2[x]) - Convert.ToDouble(Feat2[y]));
+
+                        manhattan = Math.Round(manhattan, 2);
+                    }
+                }
+            }
+            #endregion
+
+            #region Euclidean
+            if (radioButtonEuclidean.Checked)
+            {
+                for (int x = 0; x < DataClass.DataMemberCount; x++)
+                {
+                    for (int y = 0; y < DataClass.DataMemberCount; y++)
+                    {
+                        euclidean =
+                        Math.Pow(Convert.ToDouble(Feat1[x]) - Convert.ToDouble(Feat1[y]), 2) +
+                        Math.Pow(Convert.ToDouble(Feat2[x]) - Convert.ToDouble(Feat2[y]), 2);
+
+                        euclidean = Math.Round(Math.Sqrt(euclidean), 2);
+                    }
+                }
+            }
+            #endregion
+
+            #region Supreme
+            if (radioButtonSupreme.Checked)
+            {
+                for (int x = 0; x < DataClass.DataMemberCount; x++)
+                {
+                    for (int y = 0; y < DataClass.DataMemberCount; y++)
+                    {
+                        double f1 = Math.Abs(Convert.ToDouble(Feat1[x]) - Convert.ToDouble(Feat1[y]));
+                        double f2 = Math.Abs(Convert.ToDouble(Feat2[x]) - Convert.ToDouble(Feat2[y]));
+                        if (f1 > f2) max = f1;
+                        else max = f2;
+
+                        supreme = max;
+                    }
+                }
+            }
+            #endregion
         }
     }
 }
